b5bc69c4b84f6e6ad4c6a59ccf5ae79eeca11589
Patrik Hrabal <patrik@Patrik--MacBook-Air.local>
2025-07-21 14:25:16 +0200

    První verze grafu a renderu

--- render.js
+++ render.js
@@ -1,10 +1,17 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+async function generateChartImage() {
+  const browser = await puppeteer.launch({
+    headless: true, // Spustí prohlížeč v pozadí
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Důležité pro GitHub Actions
+  });
   const page = await browser.newPage();
-  await page.setViewport({ width: 648, height: 480 });
-  await page.goto(`file://${__dirname}/chart.html`, { waitUntil: 'networkidle0' });
-  await page.screenshot({ path: `${__dirname}/btc-chart.png` });
+  await page.setViewport({ width: 648, height: 480, deviceScaleFactor: 2 }); // Nastavte rozlišení a DPI
+  await page.goto(`file://${path.resolve(__dirname, 'chart.html')}`, { waitUntil: 'networkidle0' });
+  await page.screenshot({ path: 'chart.png' });
   await browser.close();
-})();
+  console.log('Graf byl vygenerován jako chart.png');
+}
+
+generateChartImage();
